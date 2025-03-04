2d98f9593e1017a85860100dc7cc20302a142ee9
Sudeep Dutta <dutt.sudeep710@gmail.com>
2025-03-05 00:19:17 +0530

    Assignment Commit

--- src/main/java/com/portfolio/config/AppConfig.java
+++ src/main/java/com/portfolio/config/AppConfig.java
@@ -5,8 +5,12 @@ import com.portfolio.model.Portfolio;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.init.DatabasePopulatorUtils;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 
 import javax.sql.DataSource;
 
